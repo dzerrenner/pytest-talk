67fda75622223eca260f75bb377ba576ac7b047e
dzerrenn <david.zerrenner@t-systems.com>
2018-05-02 08:41:17 +0200

    added PASS example

--- tests/test_assertions.py
+++ tests/test_assertions.py
@@ -4,21 +4,24 @@ Foo = namedtuple('Foo', ['a', 'b', 'c'])
 Foo.__new__.__defaults__ = (None, None, False)
 
 
+def test_assertion_pass():
+    assert True
+
 def test_assertion_set():
     """
-    This demonstrates the assertion instorspection of python sets
+    This demonstrates the assertion introspection of python sets
     """
     assert {1, 2, 3} == {2, 3, 4}
 
 def test_assertion_dict():
     """
-    This demonstrates the assertion instorspection of python dicts
+    This demonstrates the assertion introspection of python dicts
     """
     assert {"a":1, "b": 2} == {"a": 2, "c": 3}
 
 def test_assertion_namedtuple():
     """
-    This demonstrates the assertion instorspection of collections.namedtuple
+    This demonstrates the assertion introspection of collections.namedtuple
     """
     foo1 = Foo("a", "b", False)
     foo2 = Foo("a", 1, False)
